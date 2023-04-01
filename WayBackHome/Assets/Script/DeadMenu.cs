cfeccf7f12f8fe3cbd15038c1cc7d258053fbdc0
debbypark99 <debbypark99@gmail.com>
2023-04-01 18:55:16 +0900

    uplodad unity files

---
+++ WayBackHome/Assets/Script/DeadMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+class DeadMenu : MonoBehaviour {
+
+    public GameObject DeadUI;
+
+    public static bool dead_state = false;
+
+    void Start()
+    {
+        DeadUI.SetActive(false);
+    }
+    void Update()
+    {
+        if(dead_state)
+        {
+            DeadUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            DeadUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Resume()
+    {
+        Debug.Log("resume button clicked");
+        dead_state = false;
+        //DeadUI.SetActive(false);
+        //Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("modified");
+        dead_state = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ WayBackHome/Assets/Script/PressTracking.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressTracking : MonoBehaviour {
+
+    public GameObject Lights;
+    public static bool tracking_donation = false;
+    // Use this for initialization
+    void Start () {
+        Lights.SetActive(true);
+	}
+
+}
